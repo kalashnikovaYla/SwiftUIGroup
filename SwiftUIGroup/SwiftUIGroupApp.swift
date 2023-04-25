8858b2c27b840bf6dd46a9514a76b929b3444460
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-04-25 14:46:00 +0500

    Initial Commit

---
+++ SwiftUIGroup/SwiftUIGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGroupApp.swift
+//  SwiftUIGroup
+//
+//  Created by sss on 25.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
